01e5833cc9547f8d338177dab2c611beb7c3f52e
OneTvPlus <107822286+OneTvPlus@users.noreply.github.com>
2022-06-19 23:10:56 -0300

    Create cadclitv.js

---
+++ cadclitv.js
@@ -0,0 +1,16 @@
+function checaLogin(){
+            var nomeLogin = document.getElementById("login").value;
+            var senhaLogin = document.getElementById("senhaL").value;
+                switch (nomeLogin && senhaLogin) {
+
+                  case 'Admin'&&'123':
+                  case 'dilene'&&'123':
+                  case 'william'&&'123':
+                  case 'Samuel'&&'123':
+                  case 'edgar'&&'123':
+                        window.location.href="go:categorias";
+                    break;
+                  default:
+                    alert('Desculpe, mas o Nome:' +nomeLogin+' E senha: '+senhaLogin+'. São inválidos.');
+                }
+        }           
