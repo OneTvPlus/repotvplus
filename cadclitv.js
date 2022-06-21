d421704a4f21e091fc188f574148137a272c666b
OneTvPlus <107822286+OneTvPlus@users.noreply.github.com>
2022-06-20 23:29:58 -0300

    Update cadclitv.js

--- cadclitv.js
+++ cadclitv.js
@@ -2,7 +2,7 @@ function checaLogin(){
             var nomeLogin = document.getElementById("login").value;
             var senhaLogin = document.getElementById("senhaL").value;
                 switch (nomeLogin && senhaLogin) {
-
+/* Clientes Pagos*/
                   case 'Admin'&&'123':
                   case 'dilene'&&'123':
                   case 'william'&&'123':
@@ -10,10 +10,12 @@ function checaLogin(){
                   case 'edgar'&&'123':
                         window.location.href="go:categorias";
                     break;
-                  case 'larissa'&&'123':
-                        window.location.href="go:gratis";
+ /* Clientes Grátis*/
+                  case 'edgar'&&'123':
+                        window.location.href="go:categorias";
                     break;
                   default:
                     alert('Desculpe, mas o Nome:' +nomeLogin+' E senha: '+senhaLogin+'. São inválidos.');
+                  break;
                 }
         }           
